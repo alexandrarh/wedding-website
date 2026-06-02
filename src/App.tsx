46e7458c0a67038712bd62290d51fe6dc2b9a704
alexandrarh <alexavndrarh@gmail.com>
2026-06-02 08:33:49 -0700

    HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import ScrollToTop from './components/ScrollToTop'
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
     <ScrollToTop />
       <Routes>
         <Route path="/" element={<Home />} />
