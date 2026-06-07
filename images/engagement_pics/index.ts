44e64f50374aafe19f7bcc0a3025caaae0c6b16f
alexandrarh <alexavndrarh@gmail.com>
2026-06-06 21:27:01 -0700

    Updates

--- images/engagement_pics/index.ts
+++
@@ -1,9 +0,0 @@
-import firstImg from './first.webp';
-import secondImg from './second.webp';
-import thirdImg from './third.webp';
-import fourthImg from './fourth.webp';
-import fifthImg from './fifth.webp';
-import sixthImg from './sixth.webp';
-import seventhImg from './seventh.webp';
-
-export { firstImg, secondImg, thirdImg, fourthImg, fifthImg, sixthImg, seventhImg };
