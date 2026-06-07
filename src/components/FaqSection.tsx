6207e31c7e602c93ba5b67be10045040cd845b9a
alexandrarh <alexavndrarh@gmail.com>
2026-06-07 15:05:24 -0700

    Updated rsvp

--- src/components/FaqSection.tsx
+++ src/components/FaqSection.tsx
@@ -1,36 +1,72 @@
-const faq_section = [
+import { ReactNode } from 'react'
+
+const faq_section: { id: string; question: string; answer: ReactNode }[] = [
   {
     id: '1',
     question: 'Where do I RSVP?',
-    answer:
-      'You should either mail your RSVP to [address], or complete it via the QR Code',
+    answer: (
+      <span>
+        You should either mail your RSVP to [address], or complete it via{' '}
+        <a
+          href="https://rsvp.alexandseamus2027.com"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-[var(--color-rose)]"
+        >
+          rsvp.alexandseamus2027.com
+        </a>
+        . Please RSVP by [date] so we can get an accurate headcount. If you have
+        any questions about RSVPing, please reach out to us at{' '}
+        <a
+          href="mailto:info@alexandseamus2027.com"
+          rel="noopener noreferrer"
+          className="text-[var(--color-rose)]"
+        >
+          info@alexandseamus2027.com
+        </a>.
+      </span>
+    ),
   },
   {
     id: '2',
     question: 'What is "Black Tie" attire?',
-    answer: 'Black tie attire is [answer].',
+    answer: (
+      <span>
+        Black tie attire is tuxedoes and floor length dresses (cocktail and formal). We expect you all to look your best (no jeans, sneakers, shorts, etc.)! If you need suggestions, or want to ask about your outfit, reach out to us at {''}
+        <a
+          href="mailto:info@alexandseamus2027.com"
+          rel="noopener noreferrer"
+          className="text-[var(--color-rose)]"
+        >
+          info@alexandseamus2027.com
+        </a>!
+      </span>
+    )
   },
   {
     id: '3',
-    question: 'How far are the venues from each other?',
-    answer:
-      'The venues are about [distance], which is [time by car], [time by public transit], or [time by walking]. The great thing about San Francisco is that the walking and public transportation are pretty reliable, so any form of transportation works! But in black tie attire, probably recommend taking an Uber, Lyft, Waymo, or your car.',
+    question: "How far are St. Dominic's and Presidio Officers' Club from each other?",
+    answer: 
+      "They're about 2.5 miles apart, which is 10-15 minutes by car, 38 minutes by public transit (take the 43 MUNI bus at the Presidio Ave and Sutter St. station), or approximately 45 minutes by walking. The great thing about San Francisco is that the walking and public transportation are pretty reliable, so any form of transportation works! But in black tie attire, probably recommend taking an Uber, Lyft, Waymo, or your car.",
   },
   {
     id: '4',
     question: 'What about kids?',
-    answer: 'Kids are allowed at the events, but in order to minimize disruption on both, we would recommend finding a babysitter. That way, you can also enjoy your time at the party!',
+    answer:
+      'Kids are allowed at the events, but in order to minimize disruption on both, we would recommend finding a babysitter. That way, you can also enjoy your time at the party!',
   },
   {
     id: '5',
     question: 'Will there be an open bar?',
-    answer: '[Place answer here when confirmed]',
+    answer:
+      'Yes, we will have an open bar at the reception (both cocktail, dinner, and dancing hours)! We do ask however that you drink responsibly and do not drink and drive. Please be sure to have a designated driver or use a rideshare service if you plan on drinking. We want everyone to have fun and be safe!',
   },
   {
     id: '6',
-    question: 'I\'m driving to San Francisco the day of. Is there parking where the events are at?',
-    answer: 'There is a parking lot, as well as ample street parking, in front and by St. Dominic\'s. [Confirm parking siutation for venue].',
-  }
+    question: "I'm driving to San Francisco the day of. Is there parking where the events are at?",
+    answer:
+      "There is a parking lot, as well as ample street parking, in front and by St. Dominic's. In the Presidio Officers' Club, there's ample parking in front of the building.",
+  },
 ]
 
 export default function DetailsCards() {
@@ -53,7 +89,7 @@ export default function DetailsCards() {
         Got some questions that may need answering? Find them below!
       </p>
 
-      <br></br>
+      <br />
 
       {faq_section.map((faq) => (
         <div key={faq.id} className="w-full max-w-4xl flex flex-col gap-2">
