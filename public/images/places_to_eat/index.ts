f87426e4206cc348cf0690d39cd997cc8fadb5e5
alexandrarh <alexavndrarh@gmail.com>
2026-06-06 20:41:35 -0700

    Removing index.ts for barrel imports

--- public/images/places_to_eat/index.ts
+++
@@ -1,6 +0,0 @@
-import local_edition from './local_edition.png';
-import sams_burgers from './sams_burgers.webp';
-import sweet_maple from './sweet_maple.webp';
-import zy_peking from './zy_peking.webp';
-
-export { local_edition, sams_burgers, sweet_maple, zy_peking };
